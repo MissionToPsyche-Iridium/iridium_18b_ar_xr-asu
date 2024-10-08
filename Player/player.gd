3e412c1ff318933b4f883ff19aa480f048f637a3
ctbackie <ctbackie@gmail.com>
2024-10-07 17:50:31 -0700

    Commit to add basic player movement

---
+++ Player/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var MAX_SPEED = 300
+@export var ACCELERATION = 100
+
+@onready var axis = 0
+
+func _physics_process(delta: float):
+	move(delta)
+	
+
+func get_input():
+	axis = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	return axis
+	
+func move(delta):
+	axis = get_input()
+	if axis != 0:
+		# Apply acceleration
+		velocity.x += axis * ACCELERATION * delta
+		# Limit velocity to max speed
+		velocity = velocity.limit_length(MAX_SPEED)
+	else:
+		pass #do nothing?
+	
+	# Move (without collisions?)
+	move_and_slide()
