d64280da92ce1e41dd1d8a9999407ed5c6bb817d
ctbackie <ctbackie@gmail.com>
2024-11-04 12:06:22 -0700

    Commit to add randomized sprites for the debris as well as random rotation of the debris

--- Debris/debris.gd
+++ Debris/debris.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-
+var rotationSpeed = randf_range(-0.05, 0.05)
 
 func _process(delta: float):
 	position.y += 2
+	rotate(rotationSpeed)
 	if position.y > 800:
 		queue_free()
--- Game/world.gd
+++ Game/world.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 var debrisScene = preload("res://Debris/debris.tscn")
+var enemyTextOne = preload("res://EnemySpriteOne.png")
+var enemyTextTwo = preload("res://EnemySpriteTwo.png")
+var enemyTextThree = preload("res://EnemySpriteThree.png")
 
 
 
@@ -9,5 +12,19 @@ func _on_timer_timeout() -> void:
 	var debrisTwo = debrisScene.instantiate()
 	debrisOne.position = Vector2(randf_range(50, 250), randf_range(-250, -50))
 	debrisTwo.position = Vector2(randf_range(250, 450), randf_range(-250, -50))
+	var rand = randi_range(1, 3)
+	if (rand == 1):
+		debrisOne.get_node("Sprite2D").texture = enemyTextOne
+	elif (rand == 2):
+		debrisOne.get_node("Sprite2D").texture = enemyTextTwo
+	else:
+		debrisOne.get_node("Sprite2D").texture = enemyTextThree
+	rand = randi_range(1, 3)
+	if (rand == 1):
+		debrisTwo.get_node("Sprite2D").texture = enemyTextOne
+	elif (rand == 2):
+		debrisTwo.get_node("Sprite2D").texture = enemyTextTwo
+	else:
+		debrisTwo.get_node("Sprite2D").texture = enemyTextThree
 	add_child(debrisOne)
 	add_child(debrisTwo)
